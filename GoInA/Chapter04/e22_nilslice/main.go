ac65c2d7ccc7e3ffb8da7239e9903711c38675d2
Joe Chen <geometryolife@gmail.com>
2021-08-19 16:30:49 +0800

    feat(GoInA/Chapter04/e22, e23): add detailed examples
    GoInA/Chapter04/e22_nilslice/main.go: nil 切片与空切片的对比。
    GoInA/Chapter04/e23_nuslice/main.go: 多个空切片的对比。

--- GoInA/Chapter04/e22_nilslice/main.go
+++ GoInA/Chapter04/e22_nilslice/main.go
@@ -1,7 +1,39 @@
 // 创建nil切片
+/*
+>>> File Link:
+../e23_nuslice/main.go
+*/
 package main
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
 
 func main() {
 	// 创建nil切片，只需在声明时不做任何初始化
 	// var slice []int
+
+	var s1 []int // nil 切片
+	s2 := make([]int, 0)
+
+	// 打印 nil 切片和空切片指向的底层数组的地址
+	fmt.Printf("s1 pointer: %+v\n", *(*reflect.SliceHeader)(unsafe.Pointer(&s1)))
+	fmt.Printf("s2 pointer: %+v\n", *(*reflect.SliceHeader)(unsafe.Pointer(&s2)))
+
+	// nil 切片和空切片指向的底层数组地址不同
+	fmt.Println(*(*reflect.SliceHeader)(unsafe.Pointer(&s1)) == *(*reflect.SliceHeader)(unsafe.Pointer(&s2)))
 }
+
+/*
+结论:
+nil 切片和空切片指向的底层数组地址不同，nil 切片不指向任何的实际地址，
+空切片指向某个底层数组的地址，无论创建多少个空切片，它们都指向同一个
+底层数组的地址，参见下一个例子。nil 切片和空切片的指针指向的底层数组
+都没有分配任何存储空间。
+>>> Execution Result:
+s1 pointer: {Data:0 Len:0 Cap:0}
+s2 pointer: {Data:824634150632 Len:0 Cap:0}
+false
+*/
--- GoInA/Chapter04/e23_nuslice/main.go
+++ GoInA/Chapter04/e23_nuslice/main.go
@@ -1,5 +1,15 @@
 // 声明空切片
+/*
+>>> File Link:
+../e22_nilslice/main.go
+*/
 package main
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
 
 func main() {
 	// 使用make创建空的整型切片
@@ -7,4 +17,22 @@ func main() {
 
 	// 使用切片字面量创建空的整型切片
 	// slice := []int{}
+
+	s1 := make([]int, 0) // 空切片 s1
+	s2 := make([]int, 0) // 空切片 s2
+	s3 := []int{}
+	s4 := []int{}
+
+	fmt.Printf("s1 pointer: %+v\n", *(*reflect.SliceHeader)(unsafe.Pointer(&s1)))
+	fmt.Printf("s2 pointer: %+v\n", *(*reflect.SliceHeader)(unsafe.Pointer(&s2)))
+	fmt.Printf("s3 pointer: %+v\n", *(*reflect.SliceHeader)(unsafe.Pointer(&s3)))
+	fmt.Printf("s4 pointer: %+v\n", *(*reflect.SliceHeader)(unsafe.Pointer(&s4)))
 }
+
+/*
+>>> Execution Result:
+s1 pointer: {Data:824634150568 Len:0 Cap:0}
+s2 pointer: {Data:824634150568 Len:0 Cap:0}
+s3 pointer: {Data:824634150568 Len:0 Cap:0}
+s4 pointer: {Data:824634150568 Len:0 Cap:0}
+*/
