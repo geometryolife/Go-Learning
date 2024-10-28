501dd1e2c3033ac14ccedda013a216b5f5712593
Joe Chen <geometryolife@gmail.com>
2024-10-29 00:41:11 +0800

    add blockchain/ch2/tcp-server

---
+++ GoBlockchainDev/ch2/tcp-server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func handle_conn(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New connect ", conn.RemoteAddr())
+	buf := make([]byte, 256)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Failed to Read", err)
+			break
+		}
+		fmt.Println(n, string(buf))
+		n, err = conn.Write(buf[:n])
+		if err != nil {
+			fmt.Println("Failed to Write", err)
+			break
+		}
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
+		log.Panic("Failed to Listen", err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Failed to Accept ", err)
+		}
+		go handle_conn(conn)
+	}
+}
